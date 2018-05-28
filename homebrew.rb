b4a1d033b35a3944ba0e807d9939146d006bfd7c
atagawa <atagawa@healthcare.ne.jp>
2018-05-28 17:59:46 +0900

    modified

--- homebrew.rb
+++ homebrew.rb
@@ -11,12 +11,13 @@ class Homebrew < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    #system "./configure", "--disable-debug",
+     #                     "--disable-dependency-tracking",
+      #                    "--disable-silent-rules",
+       #                   "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    #system "make", "install" # if this fails, try separate make/make install steps
+bin.install Dir['release/*']
   end
 
   test do
